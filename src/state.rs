137fe009abcbf11bce9db77cf3add3b10d793339
송태기 <taegi@songtaegiui-MacBookPro.local>
2022-03-05 17:58:12 +0900

    working in state.rs

---
+++ src/state.rs
@@ -0,0 +1,30 @@
+use solana_program::{
+    program_pack::{IsInitialized, Pack, Sealed},
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+use arrayref::{array_mut_ref, array_ref, array_refs, mut_array_refs};
+
+pub struct Escrow {
+    pub is_initialized: bool,
+    pub initializer_pubkey: Pubkey,
+    pub temp_token_account_pubkey: Pubkey,
+    pub initializer_token_to_receive_account_pubkey: Pubkey,
+    pub expected_amount: u64,
+}
+
+impl Sealed for Escrow {}
+
+impl IsInitialized for Escrow {
+    fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+}
+
+impl Pack for Escrow {
+    const LEN: usize = 105;
+    fn unpack_from_slice(src: &[u8]) -> Result<Self, ProgramError> {
+        
+    }
+}
